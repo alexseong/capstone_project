06e7d046e57edf9cf7b346a932f67efecec92a70
lizbug <lizsia@Elizabeths-MacBook-Pro.local>
2016-11-17 11:45:01 -0800

    update data processing

--- scrape3/data_processing.py
+++ scrape3/data_processing.py
@@ -1,29 +1,14 @@
 import pandas as pd
 import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+
 
 '''
 The functions below clean the Webrobot
 datasets which can be found at: http://webrobots.io/kickstarter-datasets/
 
 '''
-
-column_names = [u'_id', u'created_at',
-                u'data', u'id',
-                u'run_id', 'project_id',
-                'creator_id', 'creator_name',
-                'goal', 'name',
-                'slug', 'blurb',
-                'status', 'pledged',
-                'cat_name', 'cat_slug',
-                'currency', 'short_name',
-                'state', 'country',
-                'spotlight', 'staff_pick',
-                'launched_at', 'usd_pledged',
-                'backers_count', 'currency_symbol',
-                'static_usd_rate', 'state_changed_at',
-                'disable_communication', 'duration_to_launch',
-                'proj_live_duration']
-
 
 # read the entire file into a python array
 def read_file(filepath):
@@ -44,14 +29,18 @@ def read_file(filepath):
     df = pd.read_json(data_json_str)
     return df
 
-# remove columns
+
+# remove unrelated columns
 def remove_features(df):
     df = df.drop('table_id', axis=1)
     df = df.drop('robot_id', axis = 1)
     return df
 
-# extract features within nested json object ['data']
 
+## FEATURE ENGINEERING
+
+
+# extract features within nested json object ['data']
 def extract_data_features(df):
     df['status'] = df['data'].map(lambda x: x['state'])
 
@@ -65,7 +54,7 @@ def extract_data_features(df):
     df['blurb'] = df['data'].map(lambda x: x['blurb'])
 
     df['pledged'] = df['data'].map(lambda x: x['pledged'])
-    df['cat_name'] = df['data'].map(lambda x: x['category']['name'])
+    df['subcat_name'] = df['data'].map(lambda x: x['category']['name'])
     df['cat_slug'] = df['data'].map(lambda x: x['category']['slug'])
     df['currency'] = df['data'].map(lambda x: x['currency'])
 
@@ -77,6 +66,7 @@ def extract_data_features(df):
     df['staff_pick'] = df['data'].map(lambda x: x['staff_pick'])
     df['created_at'] = df['data'].map(lambda x: x['created_at'])
     df['launched_at'] = df['data'].map(lambda x: x['launched_at'])
+    df['deadline'] = df['data'].map(lambda x: x['deadline'])
     df['usd_pledged'] = df['data'].map(lambda x: x['usd_pledged'])
 
     df['backers_count'] = df['data'].map(lambda x: x['backers_count'])
@@ -87,20 +77,74 @@ def extract_data_features(df):
 
     return df
 
-# convert int64 time to datetime and find interval features
 
- def get_interval(df):
+# Create interval features
+def get_interval(df):
+
     df['created_at'] = pd.to_datetime(df.created_at, unit='s')
     df['launched_at'] = pd.to_datetime(df.launched_at,unit='s')
     df['state_changed_at'] = pd.to_datetime(df.state_changed_at,unit='s')
+    df['deadline'] = pd.to_datetime(df.deadline,unit='s')
 
     df['created_at'] = df['created_at'].map(lambda x: x.date())
     df['launched_at'] = df['launched_at'].map(lambda x: x.date())
     df['state_changed_at'] = df['state_changed_at'].map(lambda x: x.date())
+    df['deadline'] = df['deadline'].map(lambda x: x.date())
 
     # find interval features
     df['days_to_launch'] = (df['launched_at'] - df['created_at']).map(lambda x:x.days)
     df['proj_live_days'] = (df['state_changed_at'] - df['launched_at']).map(lambda x:x.days)
+
+    return df
+
+
+# Create subcription rate feature
+def get_subscriptn_rate(df):
+    '''
+    Create subscription rate,the ratio bewteen the amount pledged and project goal
+    '''
+    df['subscription_rate'] = (df['pledged'] / df['goal']).round(4)
+    df['oversubscribed'] = df['subscription_rate'] >=1.
+
+    return df
+
+
+# Create feature day of week. Monday=0, Sunday=6
+def get_dayofweek(df):
+    df['launched_dow'] = df['launched_at'].map(lambda x:x.weekday())
+    df['deadline_dow']= df['deadline'].map(lambda x:x.weekday())
+    return df
+
+# Create feature of high level categories
+def cat_name(df):
+    y = df['cat_slug'].str.split('/')
+    df['cat_name'] = y.map(lambda x:x[0])
+    return df
+
+
+# Dummify  categorical data
+def dummify_catnames(col_name, df):
+    ddf = pd.concat([df, pd.get_dummies(df[col_name], prefix='cat')], axis=1)
+    ddf = ddf.drop(col_name, axis=1)
+    return ddf
+
+
+# Create feature of launch date as a week integer
+'''
+This is used to possibly explore how recency of successful campaigns
+impact the likelihood of success
+'''
+def get_week_number(df):
+    day_zero = pd.to_datetime('2013-01-01').date()
+    start = pd.Series(day_zero)
+
+    df['week_num'] = df['launched_at'] - day_zero
+    df['week_num'] = df.week_num.map(lambda x:(x.days)/7)
+    return df
+
+def get_outcome(df):
+    df['outcome'] = df.status == 'successful'
+    df = df.drop('status', axis=1)
     return df
 #######################################################
 
@@ -126,17 +170,45 @@ def extract_category(df):
 
     return df
 
-def get_webrobot(df):
+
+def get_metadata_df(df):
     '''
-    Returns dataframe with desired features. These features
-    will be joined into the final dataset.
+    Returns dataframe with desired features.
     '''
-    return df[columns_2]
+    metadata_columns = ['created_at','status','project_id',
+     'creator_id','goal','pledged','spotlight',
+     'staff_pick', 'launched_at','deadline','usd_pledged',
+     'backers_count','static_usd_rate','state_changed_at','disable_communication',
+     'days_to_launch','proj_live_days','subscription_rate','oversubscribed',
+     'launched_dow','deadline_dow','cat_art','cat_comics',
+     'cat_crafts','cat_dance', 'cat_design','cat_fashion',
+     'cat_film & video','cat_food','cat_games',
+     'cat_journalism','cat_music','cat_photography','cat_publishing',
+     'cat_technology','cat_theater', 'week_num']
 
+    return df[metadata_columns]
 
+## US based analysis
+# extract only US-based projects
+def us_only(df):
+    udf = df[df.country =='US']
+    return udf
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # df = read_file('full_data.json')
     # df = remove_features(df)
     # df = extract_data_features(df)
     # df = get_interval(df)
+    # df = get_subscriptn_rate(df)
+    # df = get_dayofweek(df)
+    # df = dummify_catnames(col_name, df)
+    # df = get_week_number(df)
+    # df = get_outcome(df)
+
+
+#  Initialize MongoDB database and collection
+#
+# client = MongoClient()
+# db = client['ksdb']
+# collection = db['full_data']
+# tab = db['full_data'].find()
